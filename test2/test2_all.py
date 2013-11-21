93b8c5c061b22258a5ff08f574f8253833521aa0
fish <fanshunhao@126.com>
2013-11-21 11:18:42 +0800

    add test2

---
+++ test2/test2_all.py
@@ -0,0 +1,105 @@
+from z3 import *
+from itertools import *
+def p24(Input, N):
+    OP_ADD  = 0
+    OP_SUB  = 1
+    OP_MUL  = 2
+    OP_DIV  = 3
+
+    op = [ Int("op_%s" % (i)) for i in range(N - 1) ]
+    left1_array = Array('left1', IntSort(), IntSort())
+    left2_array = Array('left2', IntSort(), IntSort())
+    right_array = Array('right', IntSort(), IntSort())
+    leftp = [ Int("leftp_%s" % (i)) for i in range(N * 2 - 1) ]
+    rightp = [ Int("rightp_%s" % (i)) for i in range(N * 2 - 1) ]
+    value = Array('value', IntSort(), RealSort())
+    output = range(2 * N)
+
+
+    def get_symbol(op):
+        if (op == OP_ADD):
+            return "+"
+        if (op == OP_SUB):
+            return "-"
+        if (op == OP_MUL):
+            return "*"
+        return "/"
+
+    def some_ope_app(op, x1, x2, x3):
+        return If(op == OP_ADD, value[x1] + value[x2] == value[x3],
+               If(op == OP_SUB, value[x1] - value[x2] == value[x3],
+               If(op == OP_MUL, value[x1] * value[x2] == value[x3],
+               And(value[x1] == value[x2] * value[x3], value[x2] != 0 ))))
+
+
+    S = Solver()
+    #Posistions for determined variables and the answer
+    for i in range(N):
+        S.add(rightp[i] == i) 
+    S.add(leftp[2 * N - 2] == 2 * N - 1)
+    S.add(rightp[2 * N - 2] == 2 * N - 2)
+    #Constraint for the position
+    for i in range(2 * N - 1):
+        if (i != 2 * N - 2):
+            S.add(leftp[i] < 2 * N - 1, leftp[i] >= N)
+        S.add(rightp[i] >= 0, rightp[i] < 2 * N - 1)
+        S.add(leftp[i] > rightp[i])
+        S.add(right_array[rightp[i]] == i)
+        S.add(Or(left1_array[leftp[i]] == i, left2_array[leftp[i]] == i))
+
+    #The determined values
+    S.add(right_array[2 * N - 2] == left1_array[2 * N - 1])
+    for i in range(N):
+        S.add(value[right_array[i]] == Input[i]);
+    S.add(value[left1_array[2 * N - 1]] == 24);
+
+    #Operation correctness
+    for i in range(N, 2 * N - 1):
+        #print i
+        S.add(left1_array[i] >= 0, left1_array[i] < 2 * N)
+        S.add(left2_array[i] >= 0, left2_array[i] < 2 * N)
+        S.add(right_array[i] >= 0, right_array[i] < 2 * N)
+        S.add(left1_array[i] != left2_array[i])
+        S.add(some_ope_app(op[i - N], left1_array[i], left2_array[i], right_array[i]));
+
+    #print !S.assertions()
+    #check and output
+    r = S.check()
+    if r == unsat:
+        print("no solution")
+        return 0
+    elif r == unknown:
+        print("failed")
+        print S.reason_unknown()
+    else:
+        print("found solution")
+        return 1
+        #m = S.model()
+        #for i in range(N):
+        #    print i
+        #    output[i] = str(Input[i])
+        #for i in range(N, 2 * N - 1):
+        #    
+        #    print int(str(m.eval(left1_array[i])))
+        #    print m.eval(left2_array[i])
+            #output[m[right_array][i]]
+        #    output[int(str(m.eval(right_array[i])))] = "(" + str(output[int(str(m.eval(left1_array[i])))]) + get_symbol(int(str(m.eval(op[i - N])))) + str(output[int(str(m.eval(left2_array[i])))]) + ")"
+        #print "outputs:"
+        #print [m.eval(leftp[i]) for i in range(N * 2 - 1)]
+        #print [m.eval(rightp[i]) for i in range(N * 2 - 1)]
+        #print m[rightp]
+        #print m[value]
+        #for i in range(2 * N - 1):
+        #    print output[i]
+        #print output[2 * N - 2]
+all_num=0
+goal_num=0
+for x in combinations_with_replacement([i+1 for i in range(13)], 4):
+    all_num+=1
+    #if all_num < 1136:
+    #    continue
+    one_poker=list(x)
+    print all_num, "--", one_poker, "--", goal_num
+    goal_num+=p24(one_poker,4)
+
+print "total goal num is", goal_num
